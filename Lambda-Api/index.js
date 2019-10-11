83f112485071d0b45c864a059e6453e2edb5b172
sundios <sundios@gmail.com>
2019-10-11 18:05:03 +0200

    moving files into dags folder and creating labda-api folder

---
+++ Lambda-Api/index.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DATABASE
+});
+exports.handler = (event, context, callback) => {
+  // allows for using callbacks as finish/error-handlers
+  context.callbackWaitsForEmptyEventLoop = false;
+  const sql = "select * from kw1";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    callback(null, result)
